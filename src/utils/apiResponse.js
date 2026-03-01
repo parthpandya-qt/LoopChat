8eca0b9c21124becb1b02ee4797c36b7da3e11a2
Parth Pandya <parth.pandya1307@gmail.com>
2026-03-01 16:44:48 +0530

    all set

--- src/utils/apiResponse.js
+++
@@ -1,9 +0,0 @@
-class ApiResponse{
-    constructor(statusCode,data={},message="success"){
-        this.statusCode=statusCode,
-        this.data=data,
-        this.message=message
-    }
-}
-
-export {ApiResponse}
